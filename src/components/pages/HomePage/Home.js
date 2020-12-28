9d972ebd98eff88b41f882ca85287d69279792af
yaj-23 <yajurva.trivedi@ryerson.ca>
2020-12-28 04:00:53 -0500

    Initial Commit Complete Website - Changes Soon

---
+++ src/components/pages/HomePage/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import HeroSection from '../../HeroSection';
+import {homeObjFour, homeObjOne, homeObjThree, homeObjTwo} from './Data';
+
+function Home() {
+    return (
+        <>
+            <HeroSection {...homeObjOne} />
+            <HeroSection {...homeObjThree} />
+            <HeroSection {...homeObjTwo} />
+            <HeroSection {...homeObjFour} />
+        </>
+    )
+}
+
+export default Home;
