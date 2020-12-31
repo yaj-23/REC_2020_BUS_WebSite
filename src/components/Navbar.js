49e7ceee97004bf3c0a6388504c72db85fcd52ed
yaj-23 <yajurva.trivedi@ryerson.ca>
2020-12-31 15:10:16 -0500

    final3

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,32 +1,18 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState} from 'react';
 import {Link} from 'react-router-dom';
 import {FaBars, FaTimes, FaBus} from 'react-icons/fa';
-import { Button } from './Button';
 import './Navbar.css';
 import {IconContext} from 'react-icons/lib';
 
 function Navbar() {
 
     const [click, setClick] = useState(false);
-    const [button, setButton] = useState(true);
 
     const handleClick = () => setClick(!click);
 
     const closeMobileMenu =()=> setClick(false);
     
-    const showButton = () => {
-        if(window.innerWidth <= 960){
-            setButton(false)
-        } else {
-            setButton(true)
-        }
-    }
-
-    window.addEventListener('resize', showButton);
-
-    useEffect(() => {
-        showButton();
-    }, []);
+  
 
 
     return (
@@ -41,7 +27,7 @@ function Navbar() {
 
 
                     <div className="menu-icon" onClick={handleClick}>
-                    {click ? <FaTimes/> : <FaBars/>}  {/* line just states that if its cliccked and true, show  x else sghow bars*/}
+                    {click ? <FaTimes/> : <FaBars/>}  
                     </div>
 
                     <ul className={click ? 'nav-menu active' : 'nav-menu'}>
